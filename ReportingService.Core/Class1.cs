a1d3265cb2739e75a62700455b7b95ad3481d0f7
onwuzulike emeka <texyh@outlook.com>
2017-11-02 16:15:23 +0100

    initial commit

---
+++ ReportingService.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ReportingService.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ ReportingService.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ReportingService.Services
+{
+    public class Class1
+    {
+    }
+}
